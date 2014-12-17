2ba4eda616f522a0e115aba21cb1fb7103f7ea3a
jbranchaud <jbranchaud@gmail.com>
2014-12-17 17:36:20 -0600

    Add some tests for the removeItem array extension.

--- test/spec/test.js
+++ test/spec/test.js
@@ -10,4 +10,27 @@
       });
     });
   });
+
+  describe('helper.js', function() {
+
+    describe('Array.prototype.removeItem', function() {
+
+      it('should be able to remove an existing item from an array', function() {
+        var arr = [1,2,3,4,5];
+        assert.equal(arr.removeItem(3), 3, 'should remove and return the given value');
+        assert.equal(arr.removeItem(5), 5, 'should remove and return the given value');
+        assert.equal(arr.toString(), [1,2,4].toString(), 'the array should exclude the removed values');
+      });
+
+      it('should return null when removing a non-existing item from an array', function() {
+        var arr = [1,2,3,4,5];
+        assert.equal(arr.removeItem(6), null, 'should return null');
+        arr.removeItem(1);
+        assert.equal(arr.removeItem(1), null, 'should return null');
+      });
+
+    });
+
+  });
+
 })();
