86f18c25c6ba2d82e9b7affad317eaa4b507b335
jbranchaud <jbranchaud@gmail.com>
2014-12-17 15:33:31 -0600

    Add helper.js with helper and convenience functions for project-wide use

---
+++ js/helper.js
@@ -0,0 +1,28 @@
+// helper.js
+//
+// a collection of helper and convenience functions for PickSix
+
+(function() {
+
+  /** Array Extensions **/
+
+  // Array Remove - By John Resig (MIT Licensed)
+  // http://ejohn.org/blog/javascript-array-remove/
+  Array.prototype.remove = function(from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+  };
+
+  // Array Remove Item
+  // http://davidwalsh.name/remove-item-array-javascript
+  Array.prototype.removeItem = function(item) {
+    var index = this.indexOf(item);
+    if( index != -1 ) {
+      this.splice(index, -1);
+    }
+  };
+
+  /** end of Array Extensions **/
+
+})();
